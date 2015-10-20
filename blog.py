5c7ff3ddec4dea44e76dfb5cc78b6e9dfb15bf13
joseph-yi <josephel3yi@gmail.com>
2015-10-20 16:33:38 -0700

    well heres the blog

--- blog.py
+++ blog.py
@@ -4,6 +4,8 @@
 from flask import Flask, render_template, request, session, \
 flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
+
 
 #configurations for the blog
 DATABASE = 'blog.db'
@@ -19,7 +21,7 @@ app.config.from_object(__name__)
 
 #connect to db
 def connect_db():
-    return sqlite3.connect(app.config['DATABASE_PATH'])
+    return sqlite3.connect(app.config['DATABASE'])
 
 @app.route('/', methods = ['GET','POST'])
 def login():
@@ -28,17 +30,50 @@ def login():
         if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
             error ='Invalid Credentials. Please try again'
         else:
-            session['loggin_in'] = True
+            session['logged_in'] = True
             return redirect(url_for('main'))
     return render_template('login.html',error = error)
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args,**kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route('/add', methods = ['POST'])
+@login_required
+def add():
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("Please enter a title and text")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('insert into posts (title, post) values (?, ?)', [request.form['title'], request.form['post']])
+        g.db.commit()
+        g.db.close()
+        flash('New post created')
+        return redirect(url_for('main'))
+
+
 @app.route('/main')
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], post = row[1]) for row in cur.fetchall()]
+    g.db.close()
+
+    return render_template('main.html', posts = posts)
 
 @app.route('/logout')
 def logout():
-    session.pop('loggined_in', None)
+    session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('login'))
 
--- sql.py
+++ sql.py
@@ -9,6 +9,10 @@ with sqlite3.connect("blog.db") as connection:
     c.execute("""CREATE TABLE posts
               (title TEXT, post TEXT)""")
 
-c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
-c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
-c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
+    c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
+    c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
+    c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
+
+    c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
+    c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
+    c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
